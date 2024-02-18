26d6c16fbde390afe2b34a372c89c29d671f59b5
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-18 08:16:39 +0530

    Update __main__.py

--- sanchit/__main__.py
+++ sanchit/__main__.py
@@ -1,12 +1,13 @@
 #(©) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
 import logging
-
 from .screenshotbot import ScreenShotBot
 from .config import Config
 
 if __name__ == "__main__":
     
-    logging.basicConfig(level=logging.DEBUG if Config.DEBUG else logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S',
+                        level=logging.DEBUG if Config.DEBUG else logging.INFO)
     logging.getLogger("pyrogram").setLevel(logging.INFO if Config.DEBUG else logging.WARNING)
     
     ScreenShotBot().run()
