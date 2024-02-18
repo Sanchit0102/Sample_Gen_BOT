8c945086813f610363b8919a5bf5aa57e9983924
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-18 08:14:34 +0530

    Update screenshotbot.py

--- sanchit/screenshotbot.py
+++ sanchit/screenshotbot.py
@@ -1,22 +1,20 @@
 #(©) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
 from pyrogram import Client
+from pyrogram.types import Message
 
 from .config import Config
 from .database import Database
-
 
 class ScreenShotBot(Client):
 
     def __init__(self):
         super().__init__(
-            name = screenshotbot,
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="sanchit"
-            )
+            name=Config.BOT_NAME,
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            workers=20,
+            plugins=dict(root="sanchit")
         )
 
         self.db = Database(Config.DATABASE_URL, Config.NAME)
@@ -24,13 +22,17 @@ class ScreenShotBot(Client):
         self.CHAT_FLOOD = {}
         self.broadcast_ids = {}
 
-
     async def start(self):
         await super().start()
         me = await self.get_me()
         print(f"\n\nNew session started for {me.first_name}({me.username})")
 
-
     async def stop(self):
         await super().stop()
         print('Session stopped. Bye!!')
+
+    async def on_message(self, message: Message):
+        if message.chat.type == "private":
+            await self.handle_private_message(message)
+        elif message.chat.type in ["group", "supergroup", "channel"]:
+            await self.handle_group_message(message)
