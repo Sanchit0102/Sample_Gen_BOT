a5e0b6f240363f96be8678992508d898f6fd72ed
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-19 08:46:18 +0530

    Update settings.py

--- sanchit/plugins/settings.py
+++ sanchit/plugins/settings.py
@@ -1,10 +1,10 @@
+#(©) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
 from pyrogram import filters as  Filters
 
 from ..screenshotbot import ScreenShotBot
 from ..utils import Utilities
 
+@Client.on_message(Filters.private & Filters.command("settings"))
+async def start(client: Client, message: Message):
 
-@ScreenShotBot.on_message(Filters.private & Filters.command("settings"))
-async def start(c, m):
-
-    await Utilities.display_settings(c, m)
+    await Utilities.display_settings(client, message)
