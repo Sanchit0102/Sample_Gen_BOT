2357a17874e6271e8e480795deccbdc19e2053a9
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-19 08:52:41 +0530

    Update trim_2.py

--- sanchit/plugins/trim_2.py
+++ sanchit/plugins/trim_2.py
@@ -1,3 +1,4 @@
+#(©) 𝚂𝙰𝙽𝙲𝙷𝙸𝚃 ♛⛧
 from pyrogram import filters as  Filters
 from pyrogram.types import ForceReply
 
@@ -6,16 +7,20 @@ from ..screenshotbot import ScreenShotBot
 from ..config import Config
 
 
-@ScreenShotBot.on_message(Filters.private & Filters.reply)
+@ScreenShotBot.on_message(Filters.private & (Filters.reply | Filters.command('trim')))
 async def _(c, m):
 
-    if not m.reply_to_message.reply_markup:
+    if not isinstance(m, Message):
         return
 
-    if not isinstance(m.reply_to_message.reply_markup, ForceReply):
+    if (
+        not m.reply_to_message
+        or not m.reply_to_message.reply_markup
+        or not isinstance(m.reply_to_message.reply_markup, ForceReply)
+    ):
         return
 
     if m.reply_to_message.text.startswith('#trim_video'):
-        c.loop.create_task(Utilities().trim_fn(c, m))
+        await Utilities().trim_fn(c, m)
     else:
-        c.loop.create_task(Utilities().manual_screenshot_fn(c, m))
+        await Utilities().manual_screenshot_fn(c, m)
